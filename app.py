ce0faf317a1189a19c060f968ca99e6668e81e30
Raj Vamsee <vamsee2312@gmail.com>
2025-02-10 16:58:10 -0600

    Created the repsective APIs and tested them using Postman

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-#importing Flask
+#import statements
 from flask import Flask 
+from flask import request, jsonify
+import uuid
 
 #Create a Flask application instance 
 app = Flask(__name__)
 
-#Defining the route for our receipt processor homepage "/"
+#Our in-memory storage for receipts
+receipts_details = {}
+
+#Defining the routes for our receipt processor
 @app.route('/')
 def home():
     return "Receipt Processor Application"
+
+@app.route('/receipts/process', methods=['POST'])
+def receipt_processor():
+    """ accepts a json receipt, 
+    generates a unique receipt id, 
+    stores receipt details, 
+    returns id  """
+
+    receipt = request.get_json() #receive json receipt data
+    
+    if not receipt:
+        return jsonify({"error":"Invalid Receipt JSON"}), 400
+    
+    receipt_id = str(uuid.uuid4()) #Generate a unique id
+    receipts_details[receipt_id] = receipt #Store the receipt in memory
+    return jsonify({"id": receipt_id}), 200
+
+@app.route('/receipts/<id>/points', methods=['GET'])
+def get_points(id):
+    """ Accepts receipt ID, 
+    returns points earned for that receipt """
+
+    if id in receipts_details:
+        return jsonify({"points": 20}), 200 
+    return jsonify({"error":"Receipt not found"}), 404
 
 #run our receipt processor App only if this script is executed directly
 if __name__ == '__main__':
