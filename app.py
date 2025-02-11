e7f5facc7ab36488e010a84320548749144ff449
Raj Vamsee <vamsee2312@gmail.com>
2025-02-10 18:19:12 -0600

    Wrote logic for 2 rules so far

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 #import statements
 from flask import Flask 
 from flask import request, jsonify
-import uuid
+import uuid #this is for generating unique ids for receipts
+import re
 
-#Create a Flask application instance 
+#Create a Flask application instance which will act as our server 
 app = Flask(__name__)
 
 #Our in-memory storage for receipts
-receipts_details = {}
+receipts_details = dict()
 
 #Defining the routes for our receipt processor
+#our home page on opening on any browser
 @app.route('/')
 def home():
     return "Receipt Processor Application"
 
+#API to submit the receipt (POST)
 @app.route('/receipts/process', methods=['POST'])
 def receipt_processor():
     """ accepts a json receipt, 
@@ -30,15 +33,32 @@ def receipt_processor():
     receipts_details[receipt_id] = receipt #Store the receipt in memory
     return jsonify({"id": receipt_id}), 200
 
+#function/method to write the logic to follow rules for awarding points for a particular receipt
+def points_calculator(receipt):
+    points = 0
+
+    #Rule 1 : One point for every alphanumeric character in the retailer name.
+    retailerName = receipt.get("retailer","")
+    pattern = re.sub(r'[^a-zA-Z0-9]', '', retailerName)
+    points += len(pattern)
+
+    #Rule 2 : 50 points if the total is a round dollar amount with no cents.
+    amount = float(receipt.get("total","0"))
+    if amount.is_integer():
+        points += 50
+
+
+#API to return points for a receipt (GET)
 @app.route('/receipts/<id>/points', methods=['GET'])
 def get_points(id):
     """ Accepts receipt ID, 
     returns points earned for that receipt """
 
     if id in receipts_details:
-        return jsonify({"points": 20}), 200 
+        #temporarily returning 20 points till I come up with logic for all the rules
+        return jsonify({"points": 20}), 200  
     return jsonify({"error":"Receipt not found"}), 404
 
-#run our receipt processor App only if this script is executed directly
+#This is to run our receipt processor App
 if __name__ == '__main__':
     app.run(debug=True)
